45261bacad3a6c84b8c6e0d6e715b8a0e8aa9408
Sara Andre <hisarandre@gmail.com>
2025-01-22 22:38:05 +0100

    Init project

---
+++ shopchop-back/src/test/java/com/shopchop/back/ShopchopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopchop.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopchopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
